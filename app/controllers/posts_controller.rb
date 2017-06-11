2be4d1d9ed1caa1cba9ac7923ce614cbf88d8f91
wtsung <1130362765@qq.con>
2017-06-11 17:39:22 +0800

    add post CRUD abilities

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
+
+	def index
+		@posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page: 10)
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new post_params
+
+		if @post.save
+			redirect_to @post, notice: "Hellz yeah! Your article was successfully saved!"
+		else
+			render 'new', notice: "Oh no! I was unable to save your post."
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update post_params
+			redirect_to @post, notice: "Huzzah! Your article was successfully saved!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content, :slug)
+	end
+
+	def find_post
+		@post = Post.friendly.find(params[:id])
+	end
+end
